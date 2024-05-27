d3725ee017637ccab4029f8dc0c60f0f89c568a7
Sushmita_Bist <embeddedswlipl@gmail.com>
2024-05-27 20:32:27 +0530

    e commerce

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import { useState , useEffect} from 'react';
+import './App.css';
+import Category from './Category';
+import axios from 'axios';
+
+function App() {
+  let [finalCategory, setFinalCategory] = useState([]);
+  let [finalPro, setFinalPro] = useState([]);
+  let [catName, setCatName] = useState('');
+
+  let getCategory=()=>{
+    axios.get('https://dummyjson.com/products/categories')
+    .then((res)=>res.data)
+    .then((finalRes)=>{
+      setFinalCategory(finalRes)
+       console.log(finalRes)
+    })
+  }
+
+  let getProducts=()=>{
+    axios.get('https://dummyjson.com/products')
+    .then((proRes)=>proRes.data)
+    .then((finalRes)=>{
+      setFinalPro(finalRes.products)
+    })
+  }
+  useEffect(() => {
+    getCategory();
+    getProducts();
+  }, [])
+
+  useEffect(() => {
+    	if(catName!==""){
+        axios.get(`https://dummyjson.com/products/category/${catName}`)
+        .then((proRes) => proRes.data)
+        .then((finalRes) => {
+          setFinalPro(finalRes.products)
+        })
+      }
+  }, [catName])
+
+  let Pitems = finalPro.map((item,i)=>{
+    return(
+      <ProductItems key={i} pdata={item}/>
+    ) 
+  })
+  return (
+    <>
+    <div className='py-[40px]'>
+      <div className='max-w-[1320px] mx-auto'>
+        <h1 className='text-center text-[40px] font-bold mb -[30px]'>Our Products </h1>
+        <div className='grid grid-cols-[25%_auto] gap-[30px] text-left'> 
+          <div>
+            <Category finalCategory={finalCategory} setCatName = {setCatName}/>
+          </div>
+          <div>
+            <div className='grid grid-cols-3 gap-5'>
+              {
+                finalPro.length >= 1?
+              Pitems : 'No Product found'}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
+function ProductItems({pdata}){
+  return(
+    <div className='shadow-lg text-center pb-4'>
+                <img src={pdata.thumbnail} className='w-[100%] h-[250px]'/>
+                <h4>{pdata.title}</h4>
+                <b>Rs {pdata.price}</b>
+    </div>
+  )
+}
---
+++ src/Category.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Category = ({finalCategory, setCatName}) => {
+
+  const cat = finalCategory.map((v,i) =>{
+    console.log(v)
+        return(
+            <li onClick={()=> setCatName(v.name)} key={i} className='bg-[#ccc] p-[7px] cursor-pointer text-[20px] font-serif font-[500] mb-2 text-transform:capitalize'>
+                {v.name}
+            </li>
+        )
+    })
+  return (
+    <div>
+      <h3 className='text-[25px] font-bold p-[10px]'>Product Category</h3>
+      <ul>
+        {cat}
+      </ul>
+    </div>
+  )
+}
+
+export default Category
