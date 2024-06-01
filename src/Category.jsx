394e2aa4b49b3edbc7939fd833ff0d9502a449ff
Sushmita_Bist <embeddedswlipl@gmail.com>
2024-06-01 08:17:55 +0530

    update1

--- src/Category.jsx
+++ src/Category.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const Category = ({finalCategory, setCatName}) => {
-
-  const cat = finalCategory.map((v,i) =>{
-    console.log(v)
-        return(
-            <li onClick={()=> setCatName(v.name)} key={i} className='bg-[#ccc] p-[7px] cursor-pointer text-[20px] font-serif font-[500] mb-2 text-transform:capitalize'>
-                {v.name}
-            </li>
-        )
-    })
+const Category = ({ finalCategory, setCatName }) => {
+  const cat = finalCategory.map((v, i) => {
+    console.log(v);
+    return (
+      <li
+        onClick={() => setCatName(v.name)}
+        key={i}
+        className="bg-[#da5e7d] p-[7px] cursor-pointer text-[20px] font-serif font-[500] mb-2 text-transform:capitalize"
+      >
+        {v.name}
+      </li>
+    );
+  });
   return (
     <div>
-      <h3 className='text-[25px] font-bold p-[10px]'>Product Category</h3>
-      <ul>
-        {cat}
-      </ul>
+      <h3 className="text-[25px] font-bold p-[10px]">Product Category</h3>
+      <ul>{cat}</ul>
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
